5d2c46125b597e933557ea6dcae946616ef811a5
POlbrot <polbrot@hemmersbach.com>
2018-03-01 11:48:06 +0100

    Provide some default filter if one is not provided

--- src/js/custom-filter.js
+++ src/js/custom-filter.js
@@ -1,12 +1,17 @@
 export class CustomFilter {
 
   /**
-   * Accept the object where every key has a callback. Settings must have
-   * at least one key: "default"
+   * Accept the object where every key has a callback. Settings should have
+   * at least one key: "default". If not, dummy default filter will be provided
+   * (It will not filter out anything)
+   *
    * @param settings
    */
   constructor (settings) {
     this.settings = settings;
+    if(!this.settings.default) {
+      this.settings.default = (data) => true;
+    }
   }
   /**
    * Returns a filter callback based on the value of filterInput or default if
