6c02b84ab5ae8dabca073b89a74fee28b25257fb
POlbrot <polbrot@hemmersbach.com>
2018-03-02 11:09:40 +0100

    Group code in index/js

--- src/js/user/index.js
+++ src/js/user/index.js
@@ -1,5 +1,6 @@
 export {User}         from './user';
 export {Address}      from './address';
+export {PhoneNumber}  from './phone-number';
+
 export {UserBuilder}  from './user-builder';
 export {UserDirector} from './user-director';
-export {PhoneNumber}  from './phone-number';
