ab4ed14e8c4e8d47b258a5f5aac951f9589743b3
POlbrot <polbrot@hemmersbach.com>
2018-03-02 11:07:07 +0100

    Configure builder and director

--- src/js/user/user-builder.js
+++ src/js/user/user-builder.js
@@ -1,3 +1,143 @@
+/**
+ * Provides methods to build the User object
+ */
 export class UserBuilder {
+  /**
+   * Instantiates the new empty User
+   */
+  create () {
+    this._user = new User();
+
+    return this;
+  }
+
+  /**
+   * Instantiate the new Address object and pass parameters to it. Assign new
+   * Address to User that is being created
+   *
+   * @param {String} city
+   * @param {String} zipCode
+   * @param {String} street
+   *
+   * @returns {UserBuilder}
+   */
+  setAddress (city, zipCode, street) {
+    const address = new Address();
+
+    address.city = city;
+    address.street = street;
+    address.zipCode = zipCode;
+
+    this._user.address = address;
+
+    return this;
+  }
+
+  /**
+   * Assign firstName to the user being created
+   * @param {String} firstName
+   * @returns {UserBuilder}
+   */
+  setFirstName (firstName) {
+    this._user.firstName = firstName;
+
+    return this;
+  }
+
+  /**
+   * Assign lastName to the user being created
+   * @param {String} lastName
+   * @returns {UserBuilder}
+   */
+  setLastName (lastName) {
+    this._user.lastName = lastName;
+
+    return this;
+  }
+
+  /**
+   * Assign gender to the user being created
+   * @param {String} gender
+   * @returns {UserBuilder}
+   */
+  setGender (gender) {
+    this._user.gender = gender;
+
+    return this;
+  }
+
+  /**
+   * Assign userName to the user being created
+   * @param {String} userName
+   * @returns {UserBuilder}
+   */
+  setUserName (userName) {
+    this._user.userName = userName;
+
+    return this;
+  }
+
+  /**
+   * Assign email to the user being created
+   * @param {String} email
+   * @returns {UserBuilder}
+   */
+  setEmail (email) {
+    this._user.email = email;
+
+    return this;
+  }
+
+  /**
+   * Assign password to the user being created
+   * @param {String} password
+   * @returns {UserBuilder}
+   */
+  setPassword (password) {
+    this._user.password = password;
+
+    return this;
+  }
+
+  /**
+   * Assign (push to the array) new phoneNumber to the user being created
+   * @param {String} phoneNumber
+   * @returns {UserBuilder}
+   */
+  setPhoneNumber (phoneNumber) {
+    this._user.phoneNumbers.push(phoneNumber);
+
+    return this;
+  }
+
+  /**
+   * Assign avatar to the user being created
+   * @param {String} avatar
+   * @returns {UserBuilder}
+   */
+  setAvatar (avatar) {
+    this._user.avatar = avatar;
+
+    return this;
+  }
+
+  /**
+   * Assign registrationDate to the user being created
+   * @param {Date} registrationDate
+   * @returns {UserBuilder}
+   */
+  setRegistrationDate (registrationDate) {
+    this._user.registrationDate = registrationDate;
+
+    return this;
+  }
+
+  /**
+   * Returns the User object that has been built
+   * @returns {User}
+   */
+  build () {
+    return this._user;
+  }
 
 }
--- src/js/user/user-director.js
+++ src/js/user/user-director.js
@@ -1,3 +1,22 @@
+/**
+ * Supervises the process of building the User
+ */
 export class UserDirector {
+  constructor (builder) {
+    this.builder = builder;
+  }
 
+  /**
+   * Builds the user using builder and provided data
+   * @param data
+   * @returns {User}
+   */
+  buildUser(data){
+    return this.builder
+      .create()
+      .setFirstName('Imię')
+      .setLastName('Nazwisko')
+      .setAddress('Miasto', '12-345', 'Adresowa 1/1')
+      .build();
+  }
 }
