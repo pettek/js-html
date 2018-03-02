ea75fd4422ea4271db5daa904974b0b19b352c5c
POlbrot <polbrot@hemmersbach.com>
2018-03-02 10:27:14 +0100

    Add PhoneNumber class

--- src/js/user/index.js
+++ src/js/user/index.js
@@ -2,3 +2,4 @@ export {User} from './user';
 export {Address} from './address';
 export {UserBuilder} from './user-builder';
 export {UserDirector} from './user-director';
+export {PhoneNumber} from './phone-number';
