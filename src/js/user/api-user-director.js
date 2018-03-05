31a6768a8462b1aea5522e9cef8c3a5b3fab69be
POlbrot <polbrot@hemmersbach.com>
2018-03-05 11:19:10 +0100

    Validate AND fill with defaults

--- src/js/user/api-user-director.js
+++ src/js/user/api-user-director.js
@@ -9,53 +9,92 @@ export class APIUserDirector {
     this.requestManager = requestManager;
   }
 
+  /**
+   * Checks that API sent back and make it feasible for getUser method to run
+   * If there are some data missing, make them empty strings
+   * @param object
+   * @returns {*}
+   */
   static validateJSONObject (object) {
-    return new Promise((resolve, reject) => {
-      if (!object) {
-        reject('JSON returned from API invalid');
-      }
 
-      if (!object.name || !object.name.first || !object.name.last) {
-        reject('Invalid name data from API');
-      }
+    if (!object) {
+      object = {};
+      console.error('JSON returned from API invalid');
+    }
 
-      if (!object.gender) {
-        reject('Invalid gender data from API');
-      }
+    if (!object.name || !object.name.first || !object.name.last) {
+      object.name = {
+        first: object.name.first || '',
+        last: object.name.last || '',
+      };
 
-      if (!object.login || !object.login.username || !object.login.salt ||
-        !object.login.sha256) {
-        reject('Invalid login data from API');
-      }
+      console.error('Invalid name data from API');
+    }
 
-      if (!object.location || !object.location.city ||
-        !object.location.postcode || !object.location.street) {
-        reject('Invalid location data from API');
-      }
+    if (!object.gender) {
+      object.gender = '';
 
-      if (!object.email) {
-        reject('Invalid email data from API');
-      }
+      console.error('Invalid gender data from API');
+    }
 
-      if (!object.cell) {
-        reject('Invalid cell phone data from API');
-      }
+    if (!object.login || !object.login.username || !object.login.salt ||
+      !object.login.sha256) {
 
-      if (!object.phone) {
-        reject('Invalid phone data from API');
-      }
+      object.login = {
+        username: object.login.username || '',
+        salt: object.login.salt || '',
+        sha256: object.login.sha256 || '',
+      };
 
-      if (!object.registered) {
-        reject('Invalid registation date data from API');
-      }
+      console.error('Invalid login data from API');
+    }
 
-      if (!object.picture || !object.picture.large) {
-        reject('Invalid avatar data from API');
-      }
+    if (!object.location || !object.location.city ||
+      !object.location.postcode || !object.location.street) {
 
-      resolve();
+      object.location = {
+        city: object.login.city || '',
+        postcode: object.login.postcode || '',
+        street: object.login.street || '',
+      };
 
-    });
+      console.error('Invalid location data from API');
+    }
+
+    if (!object.email) {
+      object.email = '';
+
+      console.error('Invalid email data from API');
+    }
+
+    if (!object.cell) {
+      object.cell = '';
+
+      console.error('Invalid cell phone data from API');
+    }
+
+    if (!object.phone) {
+      object.phone = '';
+
+      console.error('Invalid phone data from API');
+    }
+
+    if (!object.registered) {
+      object.registered = '';
+
+      console.error('Invalid registation date data from API');
+    }
+
+    if (!object.picture || !object.picture.large) {
+
+      object.picture = {
+        large: object.picture.large || '',
+      };
+
+      console.error('Invalid avatar data from API');
+    }
+
+    return object;
   }
 
   static parseAPIResponse (data) {
@@ -71,37 +110,32 @@ export class APIUserDirector {
     return new Promise((resolve, reject) => {
       this.requestManager.get(endpoint).then((userData) => {
 
-        // Get data as a parsed object from the API
-        const object = APIUserDirector.parseAPIResponse(userData);
+        // Get data as a parsed object from the API and validate it
+        const object = APIUserDirector.validateJSONObject(
+          APIUserDirector.parseAPIResponse(userData)
+        );
 
+        // Start with the empty User
+        const user = this.builder.create();
 
-        // Validate very thoroughly what API sent back
-        APIUserDirector.validateJSONObject(object).then(() => {
+        // Recipe for building a User
+        user.setFirstName(object.name.first).
+             setLastName(object.name.last).
+             setGender(object.gender).
+             setUserName(object.login.username).
+             setPassword(object.login.sha256).
+             setSalt(object.login.salt).
+             setAddress(object.location.city,
+               object.location.postcode,
+               object.location.street).
+             setEmail(object.email).
+             setPhoneNumber('cell', object.cell).
+             setPhoneNumber('phone', object.phone).
+             setAvatar(object.picture.large).
+             setRegistrationDate(Date(object.registered));
 
-          // Start with the empty User
-          const user = this.builder.create();
-
-          // Recipe for building a User
-          user.setFirstName(object.name.first).
-               setLastName(object.name.last).
-               setGender(object.gender).
-               setUserName(object.login.username).
-               setPassword(object.login.sha256).
-               setSalt(object.login.salt).
-               setAddress(object.location.city,
-                 object.location.postcode,
-                 object.location.street).
-               setEmail(object.email).
-               setPhoneNumber('cell', object.cell).
-               setPhoneNumber('phone', object.phone).
-               setAvatar(object.picture.large).
-               setRegistrationDate(Date(object.registered));
-
-          // And resolve the promise with it
-          resolve(user.build());
-
-        }).catch(error => reject(error));
-
+        // And resolve the promise with it
+        resolve(user.build());
 
       }).catch((error => {
 
