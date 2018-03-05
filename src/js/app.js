6798cd878451363f1eb24b31b3f3378cdea37ae3
POlbrot <polbrot@hemmersbach.com>
2018-03-05 10:07:04 +0100

    Clean up src/js/app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,4 @@
-import { Request }                       from './request/request';
-import { UserResultsHandler }            from './results-handler/user-results-handler';
-import { CustomFilter, FILTER_SETTINGS } from './filter';
-import { APIUserDirector }  from './user';
-
-// Specify API url and endpoints
-const API = 'https://randomuser.me';
-const ENDPOINT = 'api';
+import { UserController }                from './controllers/user-controller';
 
 /**
  * Class representing the application
@@ -17,9 +10,7 @@ export class App {
    */
   constructor (root) {
     this.root = root;
-    this.requestHandler = new Request(API);
-    this.filter = new CustomFilter(FILTER_SETTINGS);
-    this.usersAPIHandler = new APIUserDirector(this.requestHandler);
+    this.userController = new UserController(this.root);
   }
 
   /**
@@ -28,51 +19,23 @@ export class App {
   assignElements () {
     this.callBtn = this.root.querySelector('.request-btn');
     this.clearBtn = this.root.querySelector('.clear-btn');
-    this.userNumInput = this.root.querySelector('[name="users-number"]');
-    this.resultsContainer = this.root.querySelector('.api-results');
-    this.spinnerContainer = this.root.querySelector('.spinner');
   }
 
   /**
-   * Create all dependencies and events that will enable for this app to run
-   * requests to a specific API
+   * Create all events that will enable for this app to run requests to a
+   * specific API
    */
   run () {
     this.assignElements();
-    this.resultsHandler =
-      new UserResultsHandler(this.resultsContainer, this.spinnerContainer);
 
     // Bind click to making multiple requests to API and rendering results
     this.callBtn.addEventListener('click', () => {
-
-      // Create a filter for gender based on radio buttons value
-      const genderFilter = this.filter.chooseFilter(
-        this.root.querySelector('[name="filter"]:checked').value);
-
-      // Start from the empty container and show the loader
-      this.resultsHandler.clear().loaderOn();
-
-      const promiseArr = [];
-
-      // Fill the array with promises that will be resolved into array of users
-      for (let i = 0; i < this.userNumInput.value; i++) {
-        promiseArr.push(
-          this.usersAPIHandler.getUser(ENDPOINT)
-        );
-      }
-
-      // Resolve the array of promises, reject if any of the promises rejects
-      Promise.all(promiseArr).then(users => {
-        users.filter(genderFilter).forEach(user => {
-          this.resultsHandler.display(user);
-        });
-      }).catch((error) => console.log(error))
-        .finally(() => this.resultsHandler.loaderOff());
+      this.userController.fetchFromAPI();
     });
 
     // Clear the container if button responsible of clearing is pressed
     this.clearBtn.addEventListener('click', () => {
-      this.resultsHandler.clear();
+      this.userController.clearDisplay();
     });
   }
 }
---
+++ src/js/controllers/user-controller.js
@@ -0,0 +1,55 @@
+import { UserResultsHandler }            from '../results-handler/user-results-handler';
+import { APIUserDirector }               from '../user';
+import { Request }                       from '../request/request';
+import { CustomFilter, FILTER_SETTINGS } from '../filter';
+
+const API = 'https://randomuser.me';
+const ENDPOINT = 'api';
+
+export class UserController {
+  constructor (root) {
+    this.root = root;
+    this.resultsContainer = this.root.querySelector('.api-results');
+    this.spinnerContainer = this.root.querySelector('.spinner');
+    this.userNumInput = this.root.querySelector('[name="users-number"]');
+
+    this.requestHandler = new Request(API);
+    this.filter = new CustomFilter(FILTER_SETTINGS);
+    this.usersAPIHandler = new APIUserDirector(this.requestHandler);
+
+    this.resultsHandler =
+      new UserResultsHandler(this.resultsContainer, this.spinnerContainer);
+  }
+
+  fetchFromAPI () {
+    // Create a filter for gender based on radio buttons value
+    const genderFilter = this.filter.chooseFilter(
+      this.root.querySelector('[name="filter"]:checked').value);
+
+    // Start from the empty container and show the loader
+    this.resultsHandler.clear().loaderOn();
+
+    const promiseArr = [];
+
+    // Fill the array with promises that will be resolved into array of users
+    for (let i = 0; i < this.userNumInput.value; i++) {
+      promiseArr.push(
+        this.usersAPIHandler.getUser(ENDPOINT),
+      );
+    }
+
+    // Resolve the array of promises, reject if any of the promises rejects
+    Promise.all(promiseArr)
+           .then(users => {
+              users.filter(genderFilter).forEach(user => {
+                this.resultsHandler.display(user);
+              });
+            })
+           .catch((error) => console.log(error))
+           .finally(() => this.resultsHandler.loaderOff());
+  }
+
+  clearDisplay() {
+    this.resultsHandler.clear();
+  }
+}
