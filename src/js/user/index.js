8c35cc937b44a275ecaaf89a4afe5438786918c7
POlbrot <polbrot@hemmersbach.com>
2018-03-15 13:35:34 +0100

    Add exports to user/index

--- src/js/user/index.js
+++ src/js/user/index.js
@@ -3,5 +3,8 @@ export { Address }     from './address';
 export { PhoneNumber } from './phone-number';
 
 export { UserBuilder }           from './user-builder';
+export { UserDirector }          from './user-director';
 export { CustomAPIUserDirector } from './custom-api-user-director';
 export { APIUserDirector }       from './api-user-director';
+
+export { UserFactory } from './user-factory';
